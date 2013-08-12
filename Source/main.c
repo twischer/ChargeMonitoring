bf3612adfe8917af047be00b6575eae4136a4faa
Timo Wischer <twischer@freenet.de>
2013-08-13 00:59:01 +0200

    Add formated output

--- Source/main.c
+++ Source/main.c
@@ -1,6 +1,9 @@
 #include <ctype.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include <avr/io.h>
 #include <avr/pgmspace.h>
@@ -10,29 +13,64 @@
 #include "defines.h"
 #include "lcd.h"
 
+struct FixedPoint
+{
+	int16_t integer;
+	uint16_t decimal;
+};
+
+const uint16_t MAX_REMAINING_HOURS = 999;
 
 // the current which is running now (in mA)
-volatile int32_t current = -30456;
+volatile int32_t current = -3056;
 // the remaining capacity of the batteries (in mAh)
-volatile int32_t remainingCapacity = 25678;
+volatile int32_t remainingCapacity = 600678;
 
 FILE lcd_str = FDEV_SETUP_STREAM(lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 
 
+const struct FixedPoint getFixedPoint(const int32_t value, const uint16_t divider);
+
+
 int main(void)
 {
-  lcd_init();
-  stdout = &lcd_str;
-  
-  
-  printf_P( PSTR("%07d mA "), current );
-  printf_P( PSTR("%07d mAh\n"), remainingCapacity );
-  
-  const uint32_t remainingTimeInMinutes = remainingCapacity * 60 / -current;
-  const uint32_t remainingHours = remainingTimeInMinutes / 60;
-  const uint32_t remainingMinutes = remainingTimeInMinutes % 60;
-  
-  printf_P( PSTR("Remaining time %04d:%02d h\n"), remainingHours, remainingMinutes );
+	lcd_init();
+	stdout = &lcd_str;
+	
+	while (true)
+	{
+		// benötigt über 1k weniger als printf
+		//  char test[20];
+		//  ltoa( current, test, 10 );
+		
+		const struct FixedPoint convertedCurrent = getFixedPoint(current, 1000);
+		printf_P( PSTR(" %04d,%03d A  "), convertedCurrent.integer, convertedCurrent.decimal );
+		
+		const struct FixedPoint convertedCapacity = getFixedPoint(remainingCapacity, 1000);
+		printf_P( PSTR("%04d,%03d Ah\n"), convertedCapacity.integer, convertedCapacity.decimal );
+		
+		
+		const uint32_t remainingTimeInMinutes = remainingCapacity * 60 / -current;
+		if ( (remainingTimeInMinutes / 60) <= MAX_REMAINING_HOURS)
+		{
+			const struct FixedPoint convertedTime = getFixedPoint(remainingTimeInMinutes, 60);
+			printf_P( PSTR("Remaining time: %03d:%02d h\n"), convertedTime.integer, convertedTime.decimal );
+		}
+		else
+		{
+			printf_P( PSTR("Rem. time not available\n") );
+		}
+	}
+	
+	return 0;
+}
 
-  return 0;
+
+const struct FixedPoint getFixedPoint(const int32_t value, const uint16_t divider)
+{
+	struct FixedPoint convertedValue;
+	convertedValue.integer = (int16_t)(value / divider);
+	convertedValue.decimal = abs(value) % divider;
+	
+	return convertedValue;
 }
