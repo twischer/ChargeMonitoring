b8d43956092b7549ec8736028c53d62ed53778cc
Timo Wischer <twischer@freenet.de>
2014-04-30 22:39:04 +0200

    Add some induktivits to filter the adc inputs

--- Source/defines.h
+++ Source/defines.h
@@ -13,11 +13,11 @@
 
 
 /* HD44780 LCD port connections */
-#define HD44780_RS B, 0
-#define HD44780_RW B, 1
-#define HD44780_E  B, 2
+#define HD44780_RS B, 4
+#define HD44780_RW B, 6
+#define HD44780_E  B, 5
 /* The data bits have to be not only in ascending order but also consecutive. */
-#define HD44780_D4 B, 4
+#define HD44780_D4 B, 0
 
 /* Whether to read the busy flag, or fall back to
    worst-time delays. */
